62ea11829dae8d3f7efdbdc9ce3c3964976a0dd7
Buwaneka Sudheera Thisarasinghe <you@example.comsudheerabuwaneka@gmail.com>
2019-03-18 23:36:43 +0530

    Signed the app

--- src/App.js
+++ src/App.js
@@ -14,30 +14,13 @@ import {
   TouchableOpacity,
   Linking,
 } from 'react-native';
-import QRCodeScanner from 'react-native-qrcode-scanner';
+import Scanner from './Components/Scanner'
 
 export default class App extends Component {
-  onSuccess(e) {
-    Linking
-      .openURL(e.data)
-      .catch(err => console.error('An error occured', err));
-  }
 
   render() {
     return (
-      <QRCodeScanner
-        onRead={this.onSuccess.bind(this)}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
+      <Scanner/>
     );
   }
 }
